a69a4f6a906b9850183c24897699158aa42b0b7a
Dominique Stevenson <000029897@student.thomroth.ac.uk>
2019-01-11 10:21:33 +0000

    .

---
+++ 2012OSQuestion6/2012OSQuestion6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2012OSQuestion6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int answer = 0;
+            int column = 8;
+
+            for (int i = 0; i < answer; i++)
+            {
+                int bit;
+                Console.WriteLine("enter a bit value");
+                bit = Convert.ToInt32(Console.ReadLine());
+
+                answer = answer + (column*bit);
+                column = column / 2;
+            }
+
+            if (column < 1) 
+            {
+                Console.WriteLine("decimal value is: " + answer);
+                Console.ReadLine();
+            }
+            else
+            {
+
+            }
+
+        }
+    }
+}
